78e268b5bf56180ca8c7e7ae6b58937284367462
tamyrds <78001437+tamyrds@users.noreply.github.com>
2021-05-06 07:01:52 -0300

    Comando Break

--- Mundo_2_Python/estrutura_while/desafio_64.py
+++ Mundo_2_Python/estrutura_while/desafio_64.py
@@ -3,7 +3,9 @@ digitou = 0
 soma = 0
 N = int(input("Digite um número [999 para parar]: "))
 while N != 999:
-        soma += N
-        digitou += 1
+        if N == 999:
+                break
+        N == digitou
+        soma +=N
         N = int(input("Digite um número [999 para parar]: "))
-print(f"Você digitou {digitou} números e a  soma entre eles foi {soma}")
+print(f"Você digitou {N} números e a  soma entre eles foi {soma}")
